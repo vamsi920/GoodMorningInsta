be050947f465013459f5827b6dbcc772a6544007
vamsi <vammu920@gmail.com>
2020-10-25 12:37:25 +0530

    bot upgraded for multiple user

--- GoodMorningInsta.py
+++ GoodMorningInsta.py
@@ -6,14 +6,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver import ActionChains as AC
 import time 
 import schedule
-
+TimeOfMessage = "Enter time (24hr format)"
 def send_Text():
     print("started")
-    LoginIdText = "Enter Your Email id here"
-    PasswordText = "Enter your password here"
-    TargetUser = "Name of Target User"
-    TargetMessage = "Text message to be sent "
-
+    LoginIdText = "Input your insta email id "
+    PasswordText = "Input your Insta password"
+    TargetUser = ["user1", "user2"] ####### any number users
+    TargetMessage = "Enter your message here"
     
     op = webdriver.ChromeOptions()
     op.add_argument('headless')
@@ -37,40 +36,43 @@ def send_Text():
     ##### ignoring notification button 
     # time.sleep(2)
     # driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
-
     #### Search for target user 
     messageIcon = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[2]/a')
     messageIcon.click()
-    compose = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button'))
-    )
-    compose.click()
+    for x in TargetUser:
+        
+        compose = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button'))
+        )
+        compose.click()
 
-    time.sleep(1)
-    targetUserName = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div[2]/div[1]/div/div[2]/input'))
-    )
-    targetUserName.send_keys(TargetUser)
-    SelectTargetUser = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div[2]/div[2]/div[1]/div/div[3]/button/span'))
-    )
-    SelectTargetUser.click()
-    ClickNext = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[1]/div/div[2]/div/button")
-    ClickNext.click()
+        time.sleep(1)
+    
+        targetUserName = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div[2]/div[1]/div/div[2]/input'))
+        )
+        targetUserName.send_keys(x)
+        SelectTargetUser = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div[2]/div[2]/div[1]/div/div[3]/button/span'))
+        )
+        SelectTargetUser.click()
+        ClickNext = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[1]/div/div[2]/div/button")
+        ClickNext.click()
 
-    #####compose message and send
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea'))
-    ).send_keys(TargetMessage)
+        #####compose message and send
+        time.sleep(1)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea'))
+        ).send_keys(TargetMessage)
 
-    SendMessage = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button'))
-    )
-    SendMessage.click()
-    time.sleep(4)
+        SendMessage = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button'))
+        )
+        SendMessage.click()
+        time.sleep(2)
     print("done")
     driver.quit()
-schedule.every().day.at("01:00").do(send_Text)
+schedule.every().day.at(TimeOfMessage).do(send_Text)
 
 while True:
     schedule.run_pending()
